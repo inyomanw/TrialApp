f4a5cda0b225f4d8f22eb378ebb77b5796092a66
inyomanw <inyoman.wiryanata@gmail.com>
2018-09-18 11:06:44 +0700

    [nyoman] fixed addMethod

--- app/src/main/java/com/example/publikakun/trialapp/feature/Test/TestPresenter.java
+++ app/src/main/java/com/example/publikakun/trialapp/feature/Test/TestPresenter.java
@@ -1,4 +1,115 @@
 package com.example.publikakun.trialapp.feature.Test;
 
+import com.example.publikakun.trialapp.base.api.ApiClientOther;
+import com.example.publikakun.trialapp.base.api.ApiClientTest;
+import com.example.publikakun.trialapp.base.api.ApiInterface;
+import com.example.publikakun.trialapp.model.Post;
+import com.example.publikakun.trialapp.model.loginmodel;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class TestPresenter {
+    TestView view;
+    CompositeDisposable compositeDisposable;
+
+    public TestPresenter(TestView view, CompositeDisposable compositeDisposable) {
+        this.view = view;
+        this.compositeDisposable = compositeDisposable;
+    }
+
+    void loadData() {
+        compositeDisposable.add(ApiClientTest.getClient().create(ApiInterface.class).getPosts()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new  Consumer<List<Post>>() {
+                    @Override
+                    public void accept(List<Post> posts) {
+                        try {
+                            view.onGetLoadData(posts);
+                        } catch (Exception e) {
+                            view.onError(e.getLocalizedMessage());
+                        }
+                    }
+                })
+        );
+    }
+    public void testData()
+    {
+        ApiClientTest.getClient().create(ApiInterface.class).getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<List<Post>>() {
+                    @Override
+                    public void onNext(List<Post> posts) {
+                        try {
+                            view.onGetLoadData(posts);
+                        } catch (Exception e) {
+                            view.onError(e.getLocalizedMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.onError(e.getLocalizedMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        view.onProgresBarSuccess();
+                    }
+                });
+    }
+
+
+    void addData(String a, String b, String no) {
+        compositeDisposable.add(ApiClientOther.getClient().create(ApiInterface.class).addAlamat("user3", a, a, b, b, b, no, a, no)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<loginmodel>() {
+                    @Override
+                    public void accept(loginmodel loginmodel) throws Exception {
+                        try {
+                            if (loginmodel.getStatus().equalsIgnoreCase("Success")) {
+                                view.onSuccessAddData();
+                            }
+                        } catch (Exception e) {
+                            view.onError(e.getLocalizedMessage());
+                        }
+                    }
+                }));
+    }
+    public void addAlamat(String a,String b, String no)
+    {
+        ApiClientOther.getClient().create(ApiInterface.class).addAlamat("user2",a,a,b,b,b,no,a,no)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<loginmodel>() {
+                    @Override
+                    public void onNext(loginmodel loginmodel) {
+                        try {
+                            if (loginmodel.getStatus().equalsIgnoreCase("Success")) {
+                                view.onSuccessAddData();
+                            }
+                        } catch (Exception e) {
+                            view.onError(e.getLocalizedMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.onError(e.getLocalizedMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        view.onProgresBarSuccess();
+                    }
+                });
+    }
 }
--- app/src/main/java/com/example/publikakun/trialapp/feature/Test/TestView.java
+++ app/src/main/java/com/example/publikakun/trialapp/feature/Test/TestView.java
@@ -1,4 +1,12 @@
 package com.example.publikakun.trialapp.feature.Test;
 
+import com.example.publikakun.trialapp.model.Post;
+
+import java.util.List;
+
 public interface TestView {
+    void onGetLoadData(List<Post> posts);
+    void onSuccessAddData();
+    void onError(String e);
+    void onProgresBarSuccess();
 }
