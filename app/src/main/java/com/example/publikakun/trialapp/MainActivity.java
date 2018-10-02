670a9fc8b04cb87e002efd0f5d873e377076a3fe
feri.investree.id <feri@investree.id>
2018-10-02 13:56:43 +0700

    [nico] add rxtextview

--- app/src/main/java/com/example/publikakun/trialapp/MainActivity.java
+++ app/src/main/java/com/example/publikakun/trialapp/MainActivity.java
@@ -4,15 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.publikakun.trialapp.feature.Test.TestActivity;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.example.publikakun.trialapp.R;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etTest=findViewById(R.id.et_test);
+        RxTextView.textChanges(etTest)
+                .debounce(1,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CharSequence>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(CharSequence charSequence) {
+                        Toast.makeText(MainActivity.this, charSequence.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     public void test(View view)
